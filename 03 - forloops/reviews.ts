69d5ca4ff4e85705231ff109c1e94d28616f0693
Joseph <josephtecson18@gmail.com>
2022-05-19 16:38:15 -0700

    minor

--- 03 - forloops/reviews.ts
+++ 03 - forloops/reviews.ts
@@ -6,5 +6,7 @@ for (let i = 0; i < reviews.length; i++) {
     total += reviews[i];
 }
 
+// this 'average' variable has to placed on below line because if above
+// the for loop, then it won't recognize the results from the conditional
 let average: number = total / reviews.length;
 console.log("Review average = " + average);
